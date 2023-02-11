5f3f1f324dac0d827dee709c6b9eb9a9a3968f1e
gunesozdogan <gunes.ozdogan95@gmail.com>
2023-02-11 14:17:41 +0300

    Display cart number on the nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,10 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
 const NavBar = () => {
+    const cart = useSelector((state) => state.cart);
+
     return (
         <div className={styles.container}>
             <div className={styles.navBar}>
@@ -21,9 +24,23 @@ const NavBar = () => {
                             </Link>
                         </li>
                         <li>
-                            <Link className={styles['nav-link']} to="cart">
-                                Cart
-                            </Link>
+                            <div
+                                className={
+                                    cart.length
+                                        ? styles['cart-container-visible']
+                                        : styles['cart-container']
+                                }
+                            >
+                                <Link className={styles['nav-link']} to="cart">
+                                    Cart
+                                </Link>
+                                <span
+                                    className={cart.length ? '' : styles.hidden}
+                                >
+                                    {cart.length || ''}
+                                    {cart.length || ''}
+                                </span>
+                            </div>
                         </li>
                     </ul>
                 </nav>
